8d228c0a75e592d25f2c2eac38cae17d0ebf487c
Calleb Joel Miquissene <42087219+callebdev@users.noreply.github.com>
2019-10-26 00:39:02 +0200

    Upload
    Uploaded the Candles Challenge

---
+++ CandyLane.java
@@ -0,0 +1,35 @@
+
+public class CandyLane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("WELCOME");
+		int [] ar = {7, 6, 7, 7};
+		
+		int candles = 0; 
+		int max = 0;
+		
+		for (int i = 0; i<ar.length; i++){
+			
+			if(ar[i]>max){
+				max = ar[i]; 
+			}	
+			
+		}
+		
+		for (int i = 0; i<ar.length; i++ ){
+			
+			if (ar[i] == max){
+				candles ++;
+			}
+			
+		}
+	
+		System.out.println(candles);
+		
+		
+		
+	}
+
+}
